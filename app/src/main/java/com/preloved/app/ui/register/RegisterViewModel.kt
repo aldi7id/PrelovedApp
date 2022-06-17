8e775a320e6729b53a05fb3b8a1b103dcf5cd37a
Arif Widayana <arifwidayana@gmail.com>
2022-06-17 23:32:14 +0700

    diff from branch feature register

--- app/src/main/java/com/preloved/app/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/preloved/app/ui/register/RegisterViewModel.kt
@@ -1,4 +1,34 @@
 package com.preloved.app.ui.register
 
-class RegisterViewModel {
-}
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.bumptech.glide.load.engine.Resource
+import com.preloved.app.base.arch.BaseViewModellmpl
+import com.preloved.app.data.network.model.request.auth.RegisterRequest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+//class RegisterViewModel(
+//    private val registerRepository: RegisterRepository
+//): BaseViewModellmpl(), RegisterContract.ViewModel {
+//    private val registerUserLiveData = MutableLiveData<Resource<User>>()
+
+//    override fun getRegisterUserLiveData(): LiveData<Resource<Lore>> = registerUserLiveData
+//
+//    override fun registerUser(registerRequest: RegisterRequest) {
+//        registerUserLiveData.value = Resource.Loading()
+//        viewModelScope.launch(Dispatchers.IO) {
+//            try {
+//                val response = registerRepository.postRegisterUser(registerRequest)
+//                viewModelScope.launch(Dispatchers.Main) {
+//                   registerUserLiveData.value = Resource.Success(response.data)
+//                }
+//            } catch (e: Exception) {
+//                viewModelScope.launch(Dispatchers.Main) {
+//                    registerUserLiveData.value = Resource.Error(null, e.message.orEmpty())
+//                }
+//            }
+//        }
+//    }
+//}
