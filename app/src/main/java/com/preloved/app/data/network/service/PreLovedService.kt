f19906c916a22d5a72ce541af243560976855bca
Arif Widayana <arifwidayana@gmail.com>
2022-06-17 23:35:46 +0700

    service network from swagger

---
+++ app/src/main/java/com/preloved/app/data/network/service/PreLovedService.kt
@@ -0,0 +1,41 @@
+package com.preloved.app.data.network.service
+
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.preloved.app.data.network.model.request.auth.LoginRequest
+import com.preloved.app.data.network.model.response.LoginResponse
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+import java.util.concurrent.TimeUnit
+
+interface PreLovedService {
+    @POST("auth/login")
+    suspend fun postLogin(@Body loginRequest: LoginRequest): LoginResponse
+
+    companion object {
+        @JvmStatic
+        operator fun invoke(chuckerInterceptor: ChuckerInterceptor): PreLovedService{
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(chuckerInterceptor)
+                .addInterceptor {
+                    val req = it.request()
+                    val query = req.url
+                        .newBuilder()
+                        .build()
+                    return@addInterceptor it.proceed(req.newBuilder().url(query).build())
+                }
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://market-final-project.herokuapp.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build()
+            return retrofit.create(PreLovedService::class.java)
+        }
+    }
+}
