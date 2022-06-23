05b251cd64290f14491914a4408578ac07de872e
Arif Widayana <arifwidayana@gmail.com>
2022-06-23 22:06:13 +0700

    just comment

--- app/src/main/java/com/preloved/app/data/network/datasource/SecondHandApi.kt
+++ app/src/main/java/com/preloved/app/data/network/datasource/SecondHandApi.kt
@@ -1,8 +1,5 @@
 package com.preloved.app.data.network.datasource
 
-import com.preloved.app.data.network.model.request.auth.RegisterRequest
-import com.preloved.app.data.network.model.response.BaseAuthResponse
-
 interface SecondHandApi {
     //suspend fun postRegisterUser(registerRequest: RegisterRequest): BaseAuthResponse<User, String>
 
