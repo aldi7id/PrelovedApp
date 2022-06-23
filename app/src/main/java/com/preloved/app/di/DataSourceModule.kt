d1290c191130548559575f53b48779b41251b17b
Arif Widayana <arifwidayana@gmail.com>
2022-06-23 21:50:36 +0700

    update fix datastore

--- app/src/main/java/com/preloved/app/di/DataSourceModule.kt
+++ app/src/main/java/com/preloved/app/di/DataSourceModule.kt
@@ -1,9 +1,16 @@
 package com.preloved.app.di
 
-import com.preloved.app.data.network.datasource.UserDataSource
+import com.preloved.app.data.local.datasource.LocalDataSource
 import com.preloved.app.data.network.datasource.UserDataSourcelmpl
+import com.preloved.app.data.local.datasource.LocalDataSourceImpl
+import com.preloved.app.data.local.datastore.DatastoreManager
+import com.preloved.app.data.network.datasource.UserDataSource
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val dataSourceModule = module {
+    single { DatastoreManager(androidContext()) }
+    single<LocalDataSource> { LocalDataSourceImpl(get()) }
     single<UserDataSource> { UserDataSourcelmpl(get()) }
 }
