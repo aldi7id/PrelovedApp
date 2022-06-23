37b76d827136accca655bd4d78315e5aa4f8e788
Arif Widayana <arifwidayana@gmail.com>
2022-06-23 21:53:25 +0700

    update login user and datastore in DI

--- app/src/main/java/com/preloved/app/di/RepositoryModule.kt
+++ app/src/main/java/com/preloved/app/di/RepositoryModule.kt
@@ -2,10 +2,12 @@ package com.preloved.app.di
 
 import com.preloved.app.ui.login.LoginRepository
 import com.preloved.app.ui.register.RegisterRepository
+import com.preloved.app.ui.splash.SplashRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val repositoryModule = module {
+    singleOf(::SplashRepository)
     singleOf(::LoginRepository)
     singleOf(::RegisterRepository)
 }
