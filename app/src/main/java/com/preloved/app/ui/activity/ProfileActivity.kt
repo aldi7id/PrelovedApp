990d3fe82ad31d0555024e0bebe8802fea889d27
Arif Widayana <arifwidayana@gmail.com>
2022-06-23 21:46:07 +0700

    add and update auth, home, main, profile,splash activity with navigation component

--- app/src/main/java/com/preloved/app/ui/activity/ProfileActivity.kt
+++ app/src/main/java/com/preloved/app/ui/activity/ProfileActivity.kt
@@ -1,11 +1,12 @@
-package com.preloved.app
+package com.preloved.app.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.preloved.app.R
 
-class MainActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
     }
 }
---
+++ app/src/main/java/com/preloved/app/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.preloved.app.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.preloved.app.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
