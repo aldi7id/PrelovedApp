86ce9c05005aa08327e9bf9b6408b407f589bf92
raflyraihan <11191053@student.itk.ac.id>
2022-07-07 13:38:16 +0800

    Upload Detail Product Fragment XML

--- app/src/main/java/com/preloved/app/PopUpTawaranFragment.kt
+++ app/src/main/java/com/preloved/app/PopUpTawaranFragment.kt
@@ -1,19 +1,19 @@
 package com.preloved.app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 
-class DetailProductFragment : Fragment() {
+class PopUpTawaranFragment : DialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_product, container, false)
+        return inflater.inflate(R.layout.fragment_pop_up_tawaran, container, false)
     }
 
 }
