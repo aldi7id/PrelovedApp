038b87dc712df96fa3b39eb5f79d7b78ba257d29
nuyuy000 <nurul.180170137@mhs.unimal.ac.id>
2022-07-15 19:57:48 +0700

    Add Feature Fragment ALL Category

--- app/src/main/java/com/preloved/app/ui/fragment/homepage/home/category/woman/shoes/CategoryShoesWomenFragment.kt
+++ app/src/main/java/com/preloved/app/ui/fragment/homepage/home/category/woman/shoes/CategoryShoesWomenFragment.kt
@@ -1,11 +1,13 @@
 package com.preloved.app.ui.fragment.homepage.home.category.woman.shoes
 
 import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import com.preloved.app.base.arch.BaseFragment
 import com.preloved.app.base.model.Resource
 import com.preloved.app.data.network.model.response.category.CategoryResponse
 import com.preloved.app.databinding.FragmentCategoryClothWomenBinding
 import com.preloved.app.databinding.FragmentCategoryShoesWomenBinding
+import com.preloved.app.ui.fragment.MainFragmentDirections
 import com.preloved.app.ui.fragment.homepage.home.category.CategoryAllAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -51,7 +53,10 @@ class CategoryShoesWomenFragment : BaseFragment<FragmentCategoryShoesWomenBindin
         viewModel.apply {
             with(getViewBinding()) {
                 val listAdapter = CategoryAllAdapter{
-
+                    val passData = MainFragmentDirections.actionMainFragmentToDetailProductFragment(
+                        productId = it.id
+                    )
+                    findNavController().navigate(passData)
                 }
                 listAdapter.submitList(data)
                 rvCategory.adapter = listAdapter
